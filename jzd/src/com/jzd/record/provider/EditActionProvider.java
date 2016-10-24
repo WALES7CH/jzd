99dcf03416bad9796a42f3c765867e8c5e50a4a3
WALES7CH <lxwzhjun@gmail.com>
2016-10-24 22:25:38 +0800

    1024-1

---
+++ jzd/src/com/jzd/record/provider/EditActionProvider.java
@@ -0,0 +1,42 @@
+package com.jzd.record.provider;
+
+import android.content.Context;
+import android.support.v4.view.ActionProvider;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.jzd.record.R;
+
+public class EditActionProvider extends ActionProvider {
+
+	private Context context;
+	private LayoutInflater inflater;
+	private View view;
+	private ImageView button;
+
+	public EditActionProvider(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		this.context = context;
+		inflater = LayoutInflater.from(context);
+		// view = inflater.inflate(R.layout.myactionprovider, null);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View onCreateActionView() {
+		// TODO Auto-generated method stub
+		// button = (ImageView) view.findViewById(R.id.button);
+		button.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(context, "是我，没错", Toast.LENGTH_SHORT).show();
+			}
+		});
+		return view;
+	}
+}
